10405197cdc9327c6b289de6191c9340f1efa5d3
DylanPews <77309014+DylanPews@users.noreply.github.com>
2021-06-27 15:23:34 -0400

    Main

---
+++ main.lua
@@ -0,0 +1,15 @@
+print("[ENGINE] : Starting")
+require("data.datamain")
+local gameId = require("data.id")
+
+print(loadstart)
+function love.load()
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  
+end
